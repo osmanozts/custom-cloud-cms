a25b750bf9f87b649c87497f6a72814991fd5e40
osman.ozts <osman.ozts@gmail.com>
2024-07-01 22:32:25 +0200

    remove unnecessary changes

--- src/backend-queries/storage/open-file.ts
+++ src/backend-queries/storage/open-file.ts
@@ -7,13 +7,6 @@ export async function openFile(path: string, filename: string, bucket: string) {
   if (error) throw error;
 
   if (data?.signedUrl) {
-    const link = document.createElement("a");
-    link.href = data.signedUrl;
-    link.target = "_blank";
-    link.rel = "noopener noreferrer";
-    link.style.display = "none";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    window.open(data.signedUrl, "_blank");
   }
 }
