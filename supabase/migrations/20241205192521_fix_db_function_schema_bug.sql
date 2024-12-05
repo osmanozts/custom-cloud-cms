5bd6091825a58f7b14371817e8221a12a0ccb967
osman.ozts <osman.ozts@gmail.com>
2024-12-05 20:30:43 +0100

    fix migration postgres issue

--- supabase/migrations/20241205192521_fix_db_function_schema_bug.sql
+++ supabase/migrations/20241205192521_fix_db_function_schema_bug.sql
@@ -1,15 +1,3 @@
-alter table "public"."employees" drop constraint "employees_profile_id_fkey";
-
-alter table "public"."employees" alter column "id" set default gen_random_uuid();
-
-alter table "public"."employees" alter column "id" drop identity;
-
-alter table "public"."employees" alter column "id" set data type uuid using "id"::uuid;
-
-alter table "public"."employees" add constraint "public_employees_profile_id_fkey" FOREIGN KEY (profile_id) REFERENCES profile(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
-
-alter table "public"."employees" validate constraint "public_employees_profile_id_fkey";
-
 set check_function_bodies = off;
 
 CREATE OR REPLACE FUNCTION public.create_employee_entry()
