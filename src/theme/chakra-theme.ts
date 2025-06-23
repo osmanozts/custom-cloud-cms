550f2884adaad90aba593b3ec180e14bed7ce8e5
osman.ozts <osman.ozts@gmail.com>
2025-06-23 10:56:19 +0200

    remove unnecessary comments

--- src/theme/chakra-theme.ts
+++ src/theme/chakra-theme.ts
@@ -1,7 +1,5 @@
-// 1. Import `extendTheme`
 import { extendTheme } from "@chakra-ui/react";
 
-// 2. Call `extendTheme` and pass your custom values
 export const customTheme = extendTheme({
   colors: {
     backgroundColor: "#ffffff",
