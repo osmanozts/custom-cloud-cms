4baf2a3083527f0d3a96f57b2304e782ce92eb65
osman.ozts <osman.ozts@gmail.com>
2026-07-04 15:09:29 +0200

    add color mode togglo to login screen

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,12 @@
-import { LoginForm } from "../../components";
-import { Container, Flex } from "@chakra-ui/react";
+import { ColorModeToggle, LoginForm } from "../../components";
+import { Box, Container, Flex } from "@chakra-ui/react";
 
 export const Login = () => {
   return (
     <Container>
+      <Box position="fixed" top={4} right={4} zIndex={10}>
+        <ColorModeToggle />
+      </Box>
       <Flex alignItems="center" height="100vh">
         <LoginForm />
       </Flex>
